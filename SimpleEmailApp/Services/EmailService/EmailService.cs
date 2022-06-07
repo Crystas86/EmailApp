2e86fc4c149ca31da513dab5f8da0a1e16b7c2c5
Ben Bal <bensimonbal@gmail.com>
2022-06-07 09:50:50 +0200

    Add project files.

---
+++ SimpleEmailApp/Services/EmailService/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SimpleEmailApp.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            // Can change to any service like gmail.com, live.com etc...  , port / 
+            smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            // Username, Password
+            smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
